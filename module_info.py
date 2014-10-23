4b773c87fd2b3b7159ede5984e76a4799dad2c39
Tony Graham <foffy123@gmail.com>
2014-10-22 19:34:10 -0700

    Updated export directory

--- module_info.py
+++ module_info.py
@@ -2,5 +2,5 @@
 # Point export_dir to the folder you will be keeping your module
 # Make sure you use forward slashes (/) and NOT backward slashes (\)
 
-export_dir = "C:/Mount&Blade Warband/Modules/Brytenwalda/" #F123 - Just changed E: to C:
+export_dir = "C:/Dev/Source/Modding/M&B/Brytenwalda/Comp/123/" #F123 - Updated for personal workflow
 #export_dir = "C:/Program Files/Mount&Blade/Modules/Native/"
